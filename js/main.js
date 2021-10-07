afa5cdf731aaefa419013f0d3ac13401255ab3cc
Antonbinom <protest_the_hero@mail.ru>
2021-10-07 22:26:23 +0300

    fix: burger-menu

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	let burgerButton = document.querySelector('.burger-button');
 	burgerButton.addEventListener('click', function () {
-		document.querySelector('.header__burger-menu').classList.toggle('header__burger-menu--hidden');
+		document.querySelector('.header__burger-menu').classList.toggle('header__burger-menu--visible');
 	});
 
 });
