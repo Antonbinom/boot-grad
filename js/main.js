fe047c5182f7f16f3e77522a87619597cb8fd3be
Antonbinom <protest_the_hero@mail.ru>
2021-10-09 22:55:54 +0300

    create: gallery

--- js/main.js
+++ js/main.js
@@ -26,4 +26,34 @@ $(document).ready(function () {
 		]
 	});
 
+	// Magnific Popup - Видео плеер
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		// disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+
+	// Magnific Popup - Фотогалерея
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			maxWidth: 500,
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function (item) {
+				// return item.el.attr('title') + '<small></small>';
+			}
+		}
+	});
 });
