3a634d71cad53d8b9ef54fc80fe82dbd8b73cb15
Antonbinom <protest_the_hero@mail.ru>
2021-10-09 16:16:46 +0300

    create: advantages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
 
+	// Burger Menu
 	let burgerButton = document.querySelector('.burger-button');
 	burgerButton.addEventListener('click', function () {
 		document.querySelector('.header__burger-menu').classList.toggle('header__burger-menu--visible');
 	});
+
+	// Silck Slider - секция Facts
+	$('.responsive').slick({
+		arrows: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: false,
+		mobileFirst: true,
+		responsive: [{
+				breakpoint: 767,
+				settings: 'unslick'
+			},
+			{
+				breakpoint: 577,
+				settings: {
+					slidesToShow: 2,
+				}
+			}
+		]
+	});
+
 });
