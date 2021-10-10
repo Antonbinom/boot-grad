6eed1e1a28a6417cd908c589c3709352f0230308
Antonbinom <protest_the_hero@mail.ru>
2021-10-10 22:20:58 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -6,15 +6,16 @@ $(document).ready(function () {
 		document.querySelector('.header__burger-menu').classList.toggle('header__burger-menu--visible');
 	});
 
-	// Silck Slider - секция Facts
-	$('.responsive').slick({
+	// Silck Slider - секция Факты
+
+	$('.facts__cards.responsive').slick({
 		arrows: false,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: false,
 		mobileFirst: true,
 		responsive: [{
-				breakpoint: 767,
+				breakpoint: 768,
 				settings: 'unslick'
 			},
 			{
@@ -24,6 +25,27 @@ $(document).ready(function () {
 				}
 			}
 		]
+	});
+
+	// Silck Slider - секция 10 Шагов
+
+	$('.steps__slider.responsive').slick({
+		arrows: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: false,
+		infinite: false,
+		responsive: [{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 2,
+			}
+		}, {
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+			}
+		}]
 	});
 
 	// Magnific Popup - Видео плеер
@@ -56,4 +78,68 @@ $(document).ready(function () {
 			}
 		}
 	});
+
+	// Кнопка появляется при пркрутке вниз
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	// При нажатии на кнопку плавный скролл вверх
+	$('.scrollToTop').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
+	// Плавный скролл якорных ссылок
+	$(function () {
+		$("a[href^='#']").click(function () {
+			var _href = $(this).attr('href');
+			$('html, body').animate({
+					scrollTop: $(_href).offset().top + 'px',
+				},
+				1000
+			);
+			return false;
+		});
+	});
+
+	// Вызов/закрытие модального окна
+	let modalButton = $('[data-toggle=modal]');
+	let closeModalButton = $('.modal-close');
+	modalButton.on('click', openModal);
+	closeModalButton.on('click', closeModal);
+
+	let modalOverlay = $('.modal__overlay');
+	let modalDialog = $('.modal__dialog');
+
+	function openModal() {
+		modalOverlay.addClass('modal__overlay--visible');
+		modalDialog.addClass('modal__dialog--visible');
+	};
+
+	function closeModal(event) {
+		event.preventDefault();
+		modalOverlay.removeClass('modal__overlay--visible');
+		modalDialog.removeClass('modal__dialog--visible');
+	};
+
+	$(document).on('keydown', function (event) {
+		if (event.code == 'Escape') {
+			modalDialog.removeClass('modal__dialog--visible');
+			modalOverlay.removeClass('modal__overlay--visible');
+			$('body').removeClass('hold');
+		}
+	});
+	//закрытие при клике на маску
+	// $('.modal').on('click', function (event) {
+	// 	modalDialog.removeClass('modal__dialog--visible');
+	// 	modalOverlay.removeClass('modal__overlay--visible');
+	// 	$('body').removeClass('hold');
+	// });
 });
---
+++ send.php
@@ -0,0 +1,55 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$phone = $_POST['phone'];
+
+$title = 'Заявка с сайта BootGrad';
+$body = "
+		<h2>Данные для обратной связи</h2>
+		<b>Телефон:</b> $phone<br>
+		";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function ($str, $level) {
+        $GLOBALS['status'][] = $str;
+    };
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    include 'logpass.php';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('antonbinom2021@gmail.com', 'Семенихин Антон'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('antonbinom@yandex.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
+
+    // Проверяем отправленность сообщения
+    if ($mail->send()) {
+        $result = "success";
+    } else {
+        $result = "error";
+    }
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('location: index.html');
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
